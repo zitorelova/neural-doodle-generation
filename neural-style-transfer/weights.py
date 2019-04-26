d5f4ab425e982aa9abbdc2814650ac733f320104
Zito Relova <zitorelova@gmail.com>
2019-04-25 18:53:49 -0700

    Add nst files

---
+++ neural-style-transfer/weights.py
@@ -0,0 +1,10 @@
+# Weights for each of the features extracted from the model
+
+style_weights = {'conv1_1': 0.8,
+                'conv2_1': 0.8,
+                'conv3_1': 0.5,
+                'conv4_1': 0.3,
+                'conv5_1': 0.1}
+
+content_weight = 1
+style_weight = 1
