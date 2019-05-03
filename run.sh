ef216f8dbf0e57e40384370b9221910b3207a1fd
Zito Relova <zitorelova@gmail.com>
2019-05-03 13:22:12 -0700

    Fix run.sh file

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 python preprocess.py
 python get_vec.py
 python sketch_vae.py
-cd neural-style-transfer
+cd style_transfer
+sh models/get_weights.sh
 python main.py --content_dir=contents --style_dir=styles
