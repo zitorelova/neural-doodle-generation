55b11b8bb21c7a41afab54935c70ed38bb203026
Zito Relova <zitorelova@gmail.com>
2019-05-03 13:09:55 -0700

    Change style transfer module name

--- neural-style-transfer/models/get_weights.sh
+++
@@ -1,2 +0,0 @@
-wget -O models/vgg_normalised.pth 'https://docs.google.com/uc?export=download&id='130lq8j_US1ruuR_7H3kr4ow29kqPgvjf
-wget -O models/decoder.pth 'https://docs.google.com/uc?export=download&id='1-J-g0zVG1ASbcS4uaHAhOPDDPgxr9RXy
